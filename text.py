6305c024c316a3bee5d632aebc976d9122b9088b
edenamara <edenamara2@gmail.com>
2021-03-30 21:31:58 +0300

    first

---
+++ text.py
@@ -0,0 +1,16 @@
+def revword(word):
+    return word[::-1].lower()
+
+
+def countword():
+    count=1
+    tx= open('text.txt','r')
+    tx=tx.read()
+    tx=tx.replace('\n',' ') 
+    tx=(tx.lower())
+    w=tx.split()
+    for i in range(1,len(w)):
+        if w[0]==revword(w[i]):
+           count = count + 1 
+    return
+countword()
